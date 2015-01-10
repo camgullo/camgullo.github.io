7a401e5073ce265d053366b0b8a104bcd4d5251b
Cam Gullo <cam@gullo.me>
2015-01-10 12:56:07 -0500

    Uploading Personal Website

---
+++ index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+<head>
+
+
+		<meta charset="utf-8">
+		<title>Cam Gullo | Portfolio</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+		<link rel="shortcut icon" href="/favicon.ico?v=2">
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css">
+		<link rel="stylesheet" href="/css/flexslider.css">
+		<link id="main" rel="stylesheet" href="/css/designr-theme-cyan2.css">
+		<link id="theme" rel="stylesheet" href="/css/designr-theme-cyan2.css">
+		<link rel="stylesheet" href="/css/style-switcher.css">
+		<link href='http://fonts.googleapis.com/css?family=Lato:400,300,300italic,400italic,700,700italic,900' rel='stylesheet' type='text/css'>
+		
+			
+		<!--[if lt IE 9]>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
+		
+		<script type="text/javascript" src="/js/libs/modernizr.min.js"></script>
+		
+		
+	</head>
+  
+	<body data-spy="scroll" data-target="#main-nav" data-offset="200">
+	
+		<div id="main-nav" class="navbar navbar-fixed-top">
+			<div class="container">
+			
+				<div class="navbar-header">
+				
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
+						<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button>
+					
+
+					<a class="navbar-brand scrollto" href="#home">Cam Gullo<br><span>Webmaster</span></a>
+
+				
+				</div>
+				
+				<div id="site-nav" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="sr-only">
+							<a href="#home" class="scrollto">Home</a>
+						</li>
+						<li>
+							<a href="#services" class="scrollto">What I do</a>
+						</li>
+						<li>
+							<a href="#about" class="scrollto">About Me</a>
+						</li>
+						<li>
+							<a href="#contact" class="scrollto">Contact Me</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+		
+
+		<header id="home" class="jumbotron">
+		
+			<div class="container">
+			
+				<div class="message-box clearfix">
+				
+					<div class="left-col">
+						<p>I'm <span>Cam Gullo</span><br/>and I'm a web</p>
+					</div>	
+					
+					<div class="right-col flexslider home-slider" style="z-index:1;">
+					
+						<ul class="slides">
+						
+							<li><p>Developer</p></li>
+							
+							<li><p>Designer</p></li>
+							
+							<li><p>Master</p></li>
+							
+						</ul>
+						
+					</div><!--End home-slider -->
+					
+				</div><!--End message-box -->
+				
+			</div><!--End container -->
+			
+		</header><!--End header -->
+
+		<!-- ==============================================
+		SERVICES
+		=============================================== -->
+		<section id="services">
+		
+			<div class="container">
+			
+				<h1 class="section-title scrollimation scale-in">The <span>Services</span> I offer<i class="icon-wrench"></i></h1>
+				
+				<div class="row services scrollimation fade-left">
+					<div class="col-md-3 text-center">
+						<a class="icon"><i class="icon-flag"></i></a>
+						<h2>SEO</h2>
+					</div>
+					<div class="col-md-3 details">
+						<p>I've got the skills to get your local business above the competition on the largest search engines and make you more visible to your customers. </p>
+					</div>
+					<div class="col-md-3 text-center">
+						<a class="icon"><i class="icon-code"></i></a>
+						<h2>Development</h2>
+					</div>
+					<div class="col-md-3 details">
+						<p>I'm proficient in PhP and JavaScript. I have experience with content management systems such as Drupal. </p>
+					</div>
+				</div><!--End row (top) -->
+				
+				<div class="row line visible-md visible-lg">
+					<div class="col-md-3"><span class="spot"></span></div>
+					<div class="col-md-3"><span class="spot"></span></div>
+					<div class="col-md-3"><span class="spot"></span></div>
+					<div class="col-md-3"><span class="spot"></span></div>
+				</div><!--End row -->
+				
+				<div class="row services bottom-row scrollimation fade-right">
+					<div class="col-md-3 text-center col-md-push-3">
+						<h2>Design</h2>
+						<a class="icon"><i class="icon-pencil"></i></a>
+					</div>
+					<div class="col-md-3 details col-md-pull-3">
+						<p>I'm skilled in clean, modern design. Crisp and fluid design means customers will spend more time viewing your content. </p>
+					</div>
+					<div class="col-md-3 text-center  col-md-push-3">
+						<h2>Debian Servers</h2>
+						<a class="icon"><i class="icon-terminal"></i></a>
+					</div>
+					<div class="col-md-3 details  col-md-pull-3">
+						<p>I own many servers, one of which this website and many others are hosted on. I also specialize in gaming servers for Minecraft, Teamspeak, and more.</p>
+					</div>
+				</div><!--End row (bottom)-->
+				
+			</div><!--End container -->
+			
+		</section><!--End services section-->
+		
+		<!-- ==============================================
+		ABOUT
+		=============================================== -->		
+		<section id="about">
+		
+			<div class="container">
+			
+				<div class="row">
+					<div class="col-sm-4 col-sm-offset-1 scrollimation fade-right">
+						<img class="img-responsive img-circle img-center" src="https://pbs.twimg.com/profile_images/378800000211976156/44eb93c7fffc5c7dab288946d18d1d8c.jpeg" alt="">
+					</div>
+					<div class="col-sm-6 col-sm-offset-1 scrollimation fade-left">
+						<h1>Hello, I am Cam Gullo...</h1>
+						<p>I'm a webmaster living in Franklin, Massachusetts. This isn't a job for me, it's a hobby. </p>
+						
+					</div>
+				</div><!--End row -->
+				
+			</div><!--End container -->
+			
+		</section><!--End about section -->
+	
+		<!-- ==============================================
+		SKILLS
+		=============================================== -->			
+		<section id="skills">
+		
+			<div class="container">
+			
+				<div class="row skills">
+
+					<div class="col-sm-6 col-md-3 text-center">
+						<span class="chart" data-percent="90"><span class="percent">90</span></span>
+						<h2 class="text-center">HTML5</h2>
+					</div>
+					<div class="col-sm-6 col-md-3 text-center">
+						<span class="chart" data-percent="65"><span class="percent">65</span></span>
+						<h2 class="text-center">PhP</h2>
+					</div>
+					<div class="col-sm-6 col-md-3 text-center">
+						<span class="chart" data-percent="55"><span class="percent">55</span></span>
+						<h2 class="text-center">Graphic Design</h2>
+					</div>
+					<div class="col-sm-6 col-md-3 text-center">
+						<span class="chart" data-percent="85"><span class="percent">85</span></span>
+						<h2 class="text-center">Snowboarding</h2>
+					</div>
+					
+				</div><!--End row -->
+				
+			</div><!--End container -->
+			
+		</section><!--End skills section -->
+	
+		<!-- ==============================================
+		PORTFOLIO
+		=============================================== -->		
+		
+
+		<!-- ==============================================
+		CONTACT
+		=============================================== -->	
+		<section  id="contact">
+		
+			<div class="container">
+			
+				<h1 class="section-title scrollimation scale-in">Keep in <span>Touch</span> with me</h1>
+				
+				<div class="row">
+				
+					<div class="col-sm-5 contact-info scrollimation fade-right">
+						
+						<p>Feel free to E-mail me with your questions by using the form.</p>
+						
+						<address>
+							Cam Gullo<br>
+							cam(at)gullo.me<br>
+							Franklin MA 02038<br>
+						</address>
+						
+						<ul class="socials">
+							<li><a href="http://twitter.com/CamGullo" target="_blank"><i class="icon-twitter"></i></a></li>
+						</ul>
+						
+					</div>
+
+
+<div id="contact-form" class="col-sm-7 scrollimation fade-left">
+<form method="post" action="index.php">
+        
+    <label class="control-label">Name</label>
+	<i class="icon-user"></i>
+	<input type="text" name="name" placeholder="My name is..." class="form-control input-lg requiredField" data-error-empty="Please enter your name" size="30" />        
+
+    <label class="control-label">Email</label>
+	<i class="icon-envelope"></i>
+	<input type="email" name="email" placeholder="Please respond at..." class="form-control input-lg requiredField" data-error-empty="Please enter your email" data-error-invalid="Invalid email address" size="30" />       
+
+    <label class="control-label">Message</label>
+	<i class="icon-comment"></i>
+	<textarea rows="5" cols="20" name="message" placeholder="I wanna talk about..." class="form-control input-lg requiredField" rows="5" data-error-empty="Please enter your message"></textarea>
+
+	<label class="control-label">*What is 2+2? (Anti-spam)</label>
+	<input name="human" placeholder="Type Here" class="form-control input-lg requiredField" data-error-empty="Please answer the question">
+
+    <button id="submit" name="submit" type="submit" value="Submit" class="btn btn-theme btn-lg" data-error-message="Error!" data-sending-message="Sending..." data-ok-message="Message Sent"><i class="icon-location-arrow"></i>Send Message</button>
+
+
+</form></div>
+
+
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = $_POST['email']; 
+    $to = 'cam@gullo.me'; 
+    $subject = 'gullo.me Contact Form';
+    $human = $_POST['human'];
+			
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+				
+	if ($_POST['submit']) {
+    if ($name != '' && $email != '') {
+        if ($human == '4') {				 
+            if (mail ($to, $subject, $body, $from)) { 
+	        echo '<p>Your message has been sent!</p>';
+	    } else { 
+	        echo '<p>Something went wrong, go back and try again!</p>'; 
+	    } 
+	} else if ($_POST['submit'] && $human != '4') {
+	    echo '<p>You answered the anti-spam question incorrectly!</p>';
+	}
+    } else {
+        echo '<p>You need to fill in all required fields!!</p>';
+    }
+}
+?>
+
+
+<!-- End contact-form -->
+				  
+				</div><!-- End row -->
+				
+			</div><!-- End container -->
+			
+		</section><!-- End contact section -->
+		
+		<!-- ==============================================
+		FOOTER
+		=============================================== -->	
+		<footer id="main-footer">
+		
+			<div class="container">
+			
+				<div class="row">
+				
+					<div class="col-sm-5 left-col">
+						<h1 class="small-logo">CAM GULLO<br><span>WEBMASTER</span></h1>
+						<ul class="footer-nav">
+							<li><a class="scrollto" href="#home">Home</a></li>
+							<li><a class="scrollto" href="#services">Services</a></li>
+							<li><a class="scrollto" href="#contact">Contact</a></li>
+						</ul>
+						<p>&copy;Copyright All Rights Reserved</p>
+					</div>
+					<div class="col-sm-2 left-col"><br><br><br>
+						<a href="http://www.dmca.com/Protection/Status.aspx?ID=c1e923c6-ba64-4a5e-aacc-9b7021f7d018" title="DMCA"> <img src ="http://images.dmca.com/Badges/dmca_protected_16_120.png?ID=c1e923c6-ba64-4a5e-aacc-9b7021f7d018"  alt="DMCA.com" /></a>
+					</div>
+					<div class="col-sm-5 right-col">
+						<h2>Who Am I?</h2>
+						<p>I am a student whom enjoys playing around on the digital landscape as well as in the real world. I love to create amazing websites using HTML5 and CSS3 for play. I also love to go snowboarding.</p>
+					</div>
+					
+				</div><!-- End row -->
+				
+			</div><!-- End container -->
+			
+		</footer><!-- End footer -->
+
+		<!-- ==============================================
+		Le Java Script
+		=============================================== -->	
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>		
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+		<script src='/js/jquery.scrollto.js'></script>
+		<script src='/js/jquery.flexslider.min.js'></script>
+		<script src='/js/twitterFetcher_v10_min.js'></script>
+		<script src='/js/jquery.masonry.min.js'></script>
+		<script src="/js/waypoints.min.js"></script>
+		<script src="/js/jquery.easypiechart.js"></script>
+		<script src="/js/jquery.backstretch.min.js"></script>
+		<script src="/js/designr.js"></script>
+		<script src="/js/style-switcher.js"></script>
+		
+	</body>
+</html>
